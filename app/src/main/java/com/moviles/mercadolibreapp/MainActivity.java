e41a50ef7d37abfd9da1595b51fb7c0d1ef7a6e9
David Osorio <davosorio404@gmail.com>
2022-03-08 17:23:14 -0500

    Reparaciones en el home

--- app/src/main/java/com/moviles/mercadolibreapp/MainActivity.java
+++ app/src/main/java/com/moviles/mercadolibreapp/MainActivity.java
@@ -2,13 +2,32 @@ package com.moviles.mercadolibreapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.moviles.mercadolibreapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = mainBinding.getRoot();
+        setContentView(view);
+        mainBinding.btnHome.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnHome:
+                Intent intent = new Intent(this,com.moviles.mercadolibreapp.HomeActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
